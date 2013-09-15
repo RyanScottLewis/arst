b6c24972b33532ce05f566ca7c2b28f042d6a868
Ryan Scott Lewis <ryan@rynet.us>
2013-09-15 16:54:03 -0400

    Add simple `ARST::Generator.generate` method to delegate to proper generator

--- lib/arst/generator.rb
+++ lib/arst/generator.rb
@@ -1,2 +1,14 @@
 require 'arst/generator/ruby'
 require 'arst/generator/c'
+
+module ARST
+  
+  module Generator
+    
+    def self.generate(generator_type, node, options={})
+      const_get(generator_type.to_s.capitalize).generate(node, options)
+    end
+    
+  end
+  
+end
